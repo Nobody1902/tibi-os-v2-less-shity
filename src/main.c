1c86f2eb365618f6a443dc7d8789f5189914fadd
qyx22122 <tibor.macek1@gmail.com>
2024-12-17 20:25:08 +0100

    Added cmpstr()

--- src/main.c
+++ src/main.c
@@ -1,10 +1,13 @@
 #include "../BareMetal/api/libBareMetal.h"
 
+int cmpstr(char* str1, char* str2, int n);
+
 void _start(void) {
 
   b_output("Hello World!\n", 13);
   
-  char text[100];
+  char text[100] = {0};
+  char goodbye[100] = "goodbye";
   int cursor = 0;
 
   for(;;) {
@@ -13,20 +16,30 @@ void _start(void) {
     // Enter
     if(keyChar == 0x1C) {
       b_output("\n", 1);
+      // Check for goodbye commend
+      if(cmpstr(text, goodbye, 100))
+        b_system(SHUTDOWN, 0, 0);
+      cursor = 0;
       continue;
     }
     // Backspace
     if(keyChar == 0x0E) {
       b_output("\b", 1);
+      if(cursor > 0) cursor--;
       continue;
     }
-    if(keyChar == 'q') {
-      b_system(SHUTDOWN, 0, 0);
-    }
 
-    cursor++;
-    text[cursor] = keyChar;
+    if(keyChar){
+      text[cursor] = keyChar;
+      cursor++;
+    }
 
     b_output(&keyChar, 1);
   }
+}
+int cmpstr(char* str1, char* str2, int n){
+  for(int i = 0; i < n; i++){
+    if(str1[i] != str2[i]) return 0;
+  }
+  return 1;
 }
